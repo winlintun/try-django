6c3755e2428b762a08798d96576f12d82f08c3bb
iceface <www.iceface@gmail.com>
2022-01-05 21:08:02 +0630

    slug field & over write save method

--- articles/models.py
+++ articles/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.text import slugify
 
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    content = models.TextField()
+    slug = models.SlugField(blank=True, null=True)
+    content = models.TextField()        
     time = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+
+
+    def save(self, *arg, **kwargs):
+        if self.slug is not None:
+            self.slug = slugify(self.title)
+        super().save(*arg, **kwargs)
