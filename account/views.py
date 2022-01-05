12de2bbc9c9c9150d7bfa67318710242f0e69c7d
iceface <www.iceface@gmail.com>
2022-01-05 19:20:24 +0630

    login via authernticationform

--- account/views.py
+++ account/views.py
@@ -1,21 +1,30 @@
+from django.contrib.auth import forms
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 
 
 def login_view(request):
     if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = authenticate(request, username=username, password=password)
-        if user is None:
-            context = {"error": "Invalid username and password"}
-            return render(request, 'account/login.html', context)
-        print(user)
-        login(request, user)
-        return redirect("/")
-    return render(request, 'account/login.html', {})
+        form = AuthenticationForm(request, data=request.POST)
+        # username = request.POST.get('username')
+        # password = request.POST.get('password')
+        # user = authenticate(request, username=username, password=password)
+        # if user is None:
+        #     context = {"error": "Invalid username and password"}
+        #     return render(request, 'account/login.html', context)
+        # print(user)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("/")
+    else:
+        form = AuthenticationForm(request)
+        context = {
+            "form": form
+        }
+    return render(request, 'account/login.html', context)
 
 def logout_view(request):
     if request.method == "POST":
